94a54bd6e2ada06b5451299836b199029e352601
sbcsu <74089858+crispylemons@users.noreply.github.com>
2025-05-05 10:55:15 -0700

    q03

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,82 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+
+TEST(GuesserTest, CorrectGuess) {
+	Guesser secret("secret");
+	ASSERT_TRUE(secret.match("secret"));
+}
+  
+TEST(GuesserTest, IncorrectGuess) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match("aecret"));
+}
+  
+TEST(GuesserTest, GuessWithLeadingWhitespaceFails) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match(" secret"));
+}
+  
+TEST(GuesserTest, GuessWithTrailingWhitespaceFails) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match("secret "));
+}
+  
+TEST(GuesserTest, GuessWithBothWhitespaceFails) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match(" secret "));
+}
+  
+TEST(GuesserTest, BruteForceLock) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match("aaaaaa")); 
+	ASSERT_FALSE(secret.match("secret")); 
+}
+
+TEST(GuesserTest, BruteForceDistanceMoreThanTwo) {
+	Guesser secret("secret");
+	ASSERT_FALSE(secret.match("aaaaaa")); 
+	ASSERT_FALSE(secret.match("secret")); 
+}
+  
+TEST(GuesserTest, BruteForceLockRemainingCount) {
+	Guesser secret("secret");
+	secret.match("aaaaaa"); 
+	ASSERT_EQ(secret.remaining(), 3); 
+  }
+  
+TEST(GuesserTest, CorrectGuessResetsRemaining1) {
+	Guesser secret("secret");
+	secret.match("aecret"); 
+	secret.match("secret"); 
+	ASSERT_EQ(secret.remaining(), 3);
+}
+
+TEST(GuesserTest, CorrectGuessResetsRemaining2) {
+	Guesser secret("secret");
+	secret.match("aecret"); 
+	secret.match("aecret"); 
+	secret.match("secret"); 
+	ASSERT_EQ(secret.remaining(), 3);
+}
+
+TEST(GuesserTest, ThreeIncorrectGuessesLock) {
+	Guesser secret("secret");
+	secret.match("aecret");
+	secret.match("aecret"); 
+	secret.match("aecret"); 
+	ASSERT_FALSE(secret.match("secret")); 
+}
+
+TEST(GuesserTest, DecrementRemainingAfterLock) {
+	Guesser secret("secret");
+	secret.match("aaaaaa"); 
+	secret.match("aecret"); 
+	secret.match("aecret"); 
+	ASSERT_EQ(secret.remaining(), 1);
+}
+
+TEST(GuesserTest, GuessTruncated) {
+	Guesser secret("secretsecretsecretsecretsecretsecret");
+	ASSERT_TRUE(secret.match("secretsecretsecretsecretsecretse"));
 }
